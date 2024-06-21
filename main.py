751c24a873b20c841a4e1d1b786e9b9d2c3ae62f
Know Scratcher <103301742+KnowScratcher@users.noreply.github.com>
2024-06-21 22:33:10 +0800

    updated

--- main.py
+++ main.py
@@ -1,114 +1,158 @@
-from PyQt5 import *
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-import sys
+#version 13S3
+import os
+try:
+    import cv2
+    import numpy as np
+    import time
+    from datetime import datetime
+    from tqdm.rich import tqdm
+    from UI_Beta2 import Ui_DefaultWindow
+except:
+    os.system('pip install opencv-python')
+    os.system('pip install tqdm')
+    os.system('pip install rich')
+    os.system('pip install matplotlib')
+    os.system('pip install scipy')
+
 import cv2
-import threading
+import numpy as np
 import time
+from datetime import datetime
+from tqdm.rich import tqdm
 
-running = True
-
-def close(e):
-    global running
-    running = False
-
-class MainPage(QtWidgets.QWidget):
-    def __init__(self) -> None:
-        super().__init__()
-        self.setObjectName("main_page")
-        self.setWindowTitle("HRMonitor v2.0")
-        self.resize(1080,820)
-
-        self.camera_open = True
-
-        self.frame = None
-
-        self.setup_camera()
-        self.setup_instruction()
-        self.setup_LCD_display()
-        self.translate()
-
-        self.fingerControl = FingerControl(self)
+def start(skipDMX, camera_select, mode):
+    
+    start_t = int(time.time())
+    cam = cv2.VideoCapture(camera_select)
+    cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+    counting = 0
+    chk_count = 0
+    mx = 0
+    mn = 255
+    FDetect = False
+    if_reset = False
+    reset = False
+    passed = False
+    beats = 0
+    bpm = 0
+    brit = -4
+    bright_fail_count = 0
+    britFailContrl = False
+    autobrit = False
+    global fatalError
+    fatalError = 0
+    aeset = False
+    while True:
+        time_now = time.time_ns()
         
-
-    # Camera
-    def setup_camera(self) -> None:
-        self.camera = QtWidgets.QLabel(self)
-        self.camera.setObjectName("camera")
-        self.camera.setGeometry(0,0,1080,720)
-        camera_thread = threading.Thread(target=self.camera_control)
-        camera_thread.start()
-    
-    def camera_control(self):
-        cap = cv2.VideoCapture(0)      # 設定攝影機鏡頭
-        if not cap.isOpened():
-            print("Cannot open camera")
-            exit()
-        while running:
-            ret, self.frame = cap.read()    # 讀取攝影機畫面
-            self.fingerControl.fingercontrol()
-            if not ret:
-                print("Cannot receive frame")
-                break
-            frame = cv2.resize(self.frame, (1080, 720))   # 改變尺寸和視窗相同
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # 轉換成 RGB
-            height, width, channel = frame.shape    # 讀取尺寸和 channel數量
-            bytesPerline = channel * width          # 設定 bytesPerline ( 轉換使用 )
-            # 轉換影像為 QImage，讓 PyQt5 可以讀取
-            img = QImage(frame, width, height, bytesPerline, QImage.Format_RGB888)
-            self.camera.setPixmap(QPixmap.fromImage(img))
-
-    # Instruction
-    def setup_instruction(self):
-        self.instruction = QtWidgets.QLabel(self)
-        self.instruction.setObjectName("insturction")
-        self.instruction.setGeometry(0,720,500,100)
-        self.instruct_font = QFont()
-        self.instruct_font.setPointSize(20)
-        self.instruction.setFont(self.instruct_font)
-
-    # LCD Display
-    def setup_LCD_display(self):
-        self.lcd = QtWidgets.QLCDNumber(self)
-        self.lcd.setObjectName("LCD")
-        self.lcd.setGeometry(880,720,200,100)
-        self.lcd.display(0)
-
-    # Translate
-    def translate(self):
-        self._translate = QCoreApplication.translate
-        self.instruction.setText(self._translate("HRMonitor","請將手指放在鏡頭上"))
-
-
-class FingerControl:
-    def __init__(self,page:MainPage) -> None:
-        self.page = page
-
-    def fingercontrol(self) -> None:
-        frame = self.page.frame
-        frame = cv2.resize(frame,(600,420))
-        avgB, avgG, avgR, avgA = cv2.mean(frame)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        bright = int(cv2.mean(gray)[0])
-        if avgR > (avgB + avgG):#avgR > 70 and 
-            if bright >= 50:
-                self.page.instruction.setText(self.page._translate("HRMonitor","正在紀錄..."))
-                self.page.instruction.setStyleSheet("color: green;")
-            else:
-                self.page.instruction.setText(self.page._translate("HRMonitor","請提高背景亮度!"))
-                self.page.instruction.setStyleSheet("color: red;")
+        run_t = round(time.time())
+        check, frm = cam.read()
+        if check:
+            cv2.imshow('Camera_Capture', frm)
         else:
-            self.page.instruction.setText(self.page._translate("HRMonitor","請將手指放在鏡頭上!"))
-            self.page.instruction.setStyleSheet("color: black;")
-        print(f"R={avgR}, G={avgG}, B={avgB}, bright={bright}, if={avgR > (avgB + avgG)}...",end="\r",flush=True)
-    
+            print('Camera Start Failed!')
+            return False
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    Form = MainPage()
+        if cv2.waitKey(1) == ord('q'):
+            print('Exiting...')
+            cv2.destroyAllWindows()
+            break
+        
+        frm = cv2.resize(frm,(600,420))
+        avgB, avgG, avgR, avgA = cv2.mean(frm)
+        #v0.6.6
+        gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
+        bright = cv2.mean(gray)[0]
 
-    Form.closeEvent = close
+        # this is int(bright)
+        if str(bright)[1] == '.':
+            bright_fixed = int(str(bright)[0])
+        elif str(bright)[2] == '.':
+            bright_fixed = int(str(bright)[0:2])
+        else:
+            bright_fixed = int(str(bright)[0:3])
+        
+        # Finger detection
+        if avgR > 70 and avgR > (avgB + avgG) and counting <= 10:
+            FDetect = True
+            counting += 0.2
+        elif avgR > 70 and avgR > (avgB + avgG) and counting > 10:
+            FDetect = True
+        else:
+            FDetect = False
+            mx = 0
+            mn = 255
+            chk_count = 30
+            passed = False
+            beats = 0
+            bpm = 0
+            if counting >= 0:
+                counting -= 2
 
-    Form.show()
-    sys.exit(app.exec_())
+        
+        if FDetect == True and reset == False:
+            if passed == False and counting >= 10:
+                start_t = run_t
+                passed = True
+            
+            if run_t - start_t == 10 and counting >= 10:
+                with open('h_std.txt', 'a', encoding='utf-8') as data:
+                    with open('test.txt', 'r', encoding='utf-8') as tmp:
+                        bright_rec = tmp.readlines()
+                        bright_rec = [float(x) for x in bright_rec]
+                        tmp.close()
+                    
+                    for i, bri in enumerate(bright_rec): # appending brightness avg of 15 sec
+                        if i >= len(bright_rec)-16:
+                            data.write(f'{avg_bri}\n')
+                        else:
+                            h_mov = 0.2*(max(bright_rec[i:i+15]) - min(bright_rec[i:i+15])) # height vertical translating amount
+                            avg_bri = np.average(bright_rec[i:i+15]) + h_mov
+                            data.write(f'{avg_bri}\n')
+                    data.close()
+                return True
+            
+            # Write the brightness values
+            if counting >= 10:
+                with open('test.txt', 'a', encoding='utf-8') as data:
+                    data.write(f'{str(bright)[:6]}\n')
+                    data.close()
+
+        else:
+            if reset == True:
+                reset = False
+                counting = 7
+            passed = False
+            with open('test.txt', 'w', encoding='utf-8') as data:
+                data.write('')
+                data.close()
+            
+            with open('h_std.txt', 'w', encoding='utf-8') as data:
+                data.write('')
+                data.close()
+
+        print(f"R: {str(avgR)[:6]}, G: {str(avgG)[:6]}, B: {str(avgB)[:6]}, A: {str(bright)[:6]}, FXL: {bright_fixed}, FD: {str(FDetect)}, S: {str(counting)[:6]}, TR: {10-(run_t - start_t)}, EXPO:{cam.get(cv2.CAP_PROP_EXPOSURE)} , ectrl: {britFailContrl}, {bright_fail_count}, {brit}, AE:{autobrit}, res:{if_reset} .....", end = "\r", flush=True)
+
+        if_reset = DMX3(time_now = time_now, skipDMX=skipDMX)
+        if if_reset == 102:
+            reset = True
+        elif if_reset == False:
+            return False
+        #檢測區塊
+
+
+def DMX3(time_now, skipDMX):
+    global fatalError
+    time_then = time.time_ns()
+    time_passed = time_then - time_now
+    if fatalError >= 5:
+        return False
+    if time_passed > 200000000:
+        print(f'fps = {10 / time_passed}')
+        print(f'偵測到效能問題(failCount = {4-fatalError})\ntime_now = {time_now}\ntime_then = {time_then}\ntime passed = {time_passed}')
+        fatalError += 1
+        return 102
+    elif time_passed == 0:
+        print('fps >= 10')
+    elif fatalError >= 0:
+        fatalError -= 0.2
